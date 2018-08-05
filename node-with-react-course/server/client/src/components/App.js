399da780b7a89b8de4ba26927a180cadfce6f14b
Joker <fs.pessina@gmail.com>
2018-08-05 20:06:47 -0300

    add routes and start implementing header

--- node-with-react-course/server/client/src/components/App.js
+++ node-with-react-course/server/client/src/components/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import  { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './Header'
+
+const Dashboard = () => (
+  <h2>Dashboard</h2>
+);
+const SurveyNew = () => (
+  <h2>SurveyNew</h2>
+);
+const Landing = () => (
+  <h2>Landing</h2>
+);
 
 const App = () => (
-  <div>
-    <p>Hello World</p>
+  <div className="container">
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route exact path="/surveys/new" component={SurveyNew} />
+      </div>
+    </BrowserRouter>
   </div>
 );
 
