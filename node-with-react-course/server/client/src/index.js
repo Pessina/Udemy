23207fdce4ceb49c637c2e3e4b21753b3e63f504
Joker <fs.pessina@gmail.com>
2018-08-05 16:16:35 -0300

    add redux

--- node-with-react-course/server/client/src/index.js
+++ node-with-react-course/server/client/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider } from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-const store = createStore(() => [], {}, applyMiddleware())
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
-  <App />
+    <App />
   </Provider>,
-  document.querySelector("#root"));
+  document.querySelector("#root")
+);
